fb439370f6701ccbfa4185d952b3b18693227bd2
Yehonatan Deri <yonatander@gmail.com>
2025-09-15 13:52:38 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,18 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder =>
+{
+    builder.WithOrigins("https://localhost:4200", "http://localhost:4200")
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+});
+
 app.MapControllers();
 
 app.Run();
